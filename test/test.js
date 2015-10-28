1241ec867240e9f0e50803cf8c8b2be5835b6512
Will Munn <willmunn@hotmail.com>
2015-10-28 13:20:08 +0000

    Basic error handling

--- test/test.js
+++ test/test.js
@@ -12,7 +12,7 @@ describe("playing highcard greater than 10", function(){
     player.bet_request(gameState, function(bet){
       assert.equal(bet, 0);
       done();
-    })
+    });
   });
 
   ['10', 'J', 'Q', 'K', 'A'].forEach((card) => {
@@ -36,7 +36,7 @@ describe("when I have a pair", function(){
     player.bet_request(gameState, function(bet){
       assert.equal(bet, gameState.current_buy_in);
       done();
-    })
+    });
   });
   it("plays a high pair all in", function(done){
     gameState.players[1].hole_cards = [
@@ -45,6 +45,15 @@ describe("when I have a pair", function(){
     player.bet_request(gameState, function(bet){
       assert.equal(bet, 999999);
       done();
-    })
+    });
   });
-})
+});
+
+describe('when things break', () => {
+    it('folds', (done) => {
+        player.bet_request({random: 'crap'}, (bet) => {
+            assert.equal(bet, 0);
+            done();
+        });
+    });
+});
